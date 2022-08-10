74ea627ccb216aaf7cf3ed88679db0cfaf6077d9
cagriyalniz <cagriyalniz@gmail.com>
2022-08-10 12:55:27 +0300

    routine func created

--- src/mini_shell/routine.c
+++ src/mini_shell/routine.c
@@ -1,6 +1,22 @@
 #include "../../include/header.h"
 
-void ft_abc()
+void routine()
 {
-	
+	int				size;
+	t_token			*token;
+	t_readline_prop	rd_prop;
+	t_word			*word;
+
+	word = malloc(sizeof(t_word));
+	token = NULL;
+	size = 0;
+	while (1)
+	{
+		rd_prop = ft_read_line();
+		token = lexical_add(ft_split(rd_prop.word->str, ' '));
+		my_add_history(rd_prop.word->str);
+		if (strncmp(rd_prop.word->str, "!lx", 3) == 0)
+			size = print_lexical(token);
+		free(rd_prop.word->str);
+	}
 }
