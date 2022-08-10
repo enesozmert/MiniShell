22dd561e1bca7598f3a0b02343a3a114219464dd
cagriyalniz <cagriyalniz@gmail.com>
2022-08-10 12:55:39 +0300

    main func changed

--- src/mini_shell/main.c
+++ src/mini_shell/main.c
@@ -5,23 +5,7 @@ int main(int argc, char const *argv[])
 	(void)argc;
 	(void)argv;
 
-	int i;
-	int size;
-	t_token *token;
-	t_readline_prop rd_prop;
+	routine();
 
-	token = NULL;
-	i = 0;
-	size = 0;
-	while (1)
-	{
-		rd_prop = ft_read_line();
-		token = lexical_add(ft_split(rd_prop.str, ' '));
-		my_add_history(rd_prop.str);
-		if (strncmp(rd_prop.str, "!lx", 3) == 0)
-			size = print_lexical(token);
-		i++;
-		free(rd_prop.str);
-	}
 	return 0;
 }
