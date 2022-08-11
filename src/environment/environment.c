178567b6f4ebc797860cf799ede41f38b1408906
cagriyalniz <cagriyalniz@gmail.com>
2022-08-11 15:35:48 +0300

    enviroment not finish

---
+++ src/environment/environment.c
@@ -0,0 +1,7 @@
+#include "../../include/header.h"
+
+char *environment()
+{
+
+	
+}
--- src/mini_shell/main.c
+++ src/mini_shell/main.c
@@ -1,10 +1,10 @@
 #include "../../include/header.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[], char **env)
 {
 	(void)argc;
 	(void)argv;
-
+	printf("	%s", env[1]);
 	routine();
 	return 0;
 }
