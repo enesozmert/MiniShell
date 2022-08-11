a5a4afea57b9d5d17e5791189a39130740d5d29b
enesozmert <en-es111@hotmail.com>
2022-08-11 23:22:36 +0300

    parser edit 4

--- src/parse/parser.c
+++ src/parse/parser.c
@@ -17,35 +17,5 @@ int parser(t_token **token, t_rdl_prop *rdl_prop)
 
 int parser_analizer(t_token *token, t_rdl_prop *rdl_prop)
 {
-	int			i;
-	int			select_i = 0;
-	t_command	commands[8];
-	
-	i = 0;
-	select_i = 0;
-	command_list(commands);
-	while (commands[i].type != NULL)
-	{
-		if (ft_strncmp(commands[i].type, token->context, ft_strlen(commands[i].type)) == 0)
-		{
-			printf("command is type => %s\n", commands[i].type);
-			select_i = i;
-		}
-		i++;
-	}
-	i = 0;
-	if (ft_strncmp(commands[select_i].type, token->context, ft_strlen(commands[select_i].type)))
-	{
-		while (i < rdl_prop->word_count)
-		{
-			if (ft_strncmp(rdl_prop->word[i].str, token->context, rdl_prop->word[i].len) == 0)
-			{
-				commands[select_i].arg = ft_strdup(rdl_prop->main_str);
-				break;
-			}
-			i++;
-		}
-	}
-	printf("command is arg => %s\n", commands[select_i].arg);
 	return (1);
 }
