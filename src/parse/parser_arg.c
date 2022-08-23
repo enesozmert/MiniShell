34dd507d471499f5c7556fb15a2fa6298a37e9a2
enesozmert <en-es111@hotmail.com>
2022-08-23 16:32:27 +0300

    parser end of

--- src/parse/parser_arg.c
+++ src/parse/parser_arg.c
@@ -1,11 +1,11 @@
 #include "../../include/header.h"
 
-void parser_arg_isoperator(char c, int *j, t_rdl *rdl)
+void parser_arg_isoperator(unsigned int c, int *j, t_rdl *rdl)
 {
 	int i;
 
 	i = *j;
-	if ((ft_isascii(c) && is_operator(rdl, c) == 0) || c == ' ')
+	if ((is_operator(rdl, c) == 0) || c == ' ')
 		rdl->buffer[i++] = c;
 	else if ((c != ' ' || c != '\t') && (i != 0))
 	{
@@ -16,19 +16,16 @@ void parser_arg_isoperator(char c, int *j, t_rdl *rdl)
 	*j = i;
 }
 
-void parser_arg_isnotoperator(char c, int *j, t_rdl *rdl)
+void parser_arg_isnotoperator(unsigned int c, int *j, t_rdl *rdl)
 {
-	int flag;
 	int i;
 
 	i = *j;
-	flag = 0;
-	if (ft_isascii(c) || c != ' ')
+	if (is_operator(rdl, c) == 0 && c != ' ')
 	{
 		rdl->buffer[i++] = c;
-		flag = 1;
 	}
-	else if ((c == ' ' || c == '\t') && (i != 0))
+	else if ((c != ' ' || c != '\t') && (i != 0))
 	{
 		rdl->buffer[i] = '\0';
 		i = 0;
@@ -49,16 +46,20 @@ void parser_arg(t_rdl *rdl)
 	int i;
 	int j;
 	int c;
+	int flag;
 
 	i = -1;
 	j = 0;
+	flag = 0;
 	while (++i < rdl->len + 1)
 	{
 		c = rdl->main_str[i];
-		if ((c == '\"' || c == '\'') && c != ' ')
-			parser_arg_isoperator(c, &j, rdl);
-		if (is_operator(rdl, c) == 0)
+		if (c == '\"' || c == '\'')
+			flag++;
+		if (flag % 2 == 0)
 			parser_arg_isnotoperator(c, &j, rdl);
+		if (flag % 2 != 0)
+			parser_arg_isoperator(c, &j, rdl);
 		if (is_operator(rdl, c))
 			parser_add_operator(rdl, c);
 	}
