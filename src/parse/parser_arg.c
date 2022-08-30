6488553be2a41c03232ec7d6fe7a2fdb7eb36409
enesozmert <en-es111@hotmail.com>
2022-08-30 18:04:26 +0300

    parser_arg

--- src/parse/parser_arg.c
+++ src/parse/parser_arg.c
@@ -67,107 +67,40 @@ void parser_arg(t_rdl *rdl)
 
 void parser_arg_split(t_rdl *rdl)
 {
-	// int i;
-	// char **redir_l;
-	// char **redir_r;
-	// int redir_l_c;
-	// int redir_r_c;
-
-	// redir_l = NULL;
-	// redir_r = NULL;
-	// redir_l_c = 0;
-	// redir_r_c = 0;
-	// if (ft_strchr(rdl->main_str, '<') != NULL && ft_strchr(rdl->main_str, '>') != NULL)
-	// {
-	// 	exit(-1);
-	// }
-	// else if (ft_strchr(rdl->main_str, '>') != NULL)
-	// {
-	// 	i = -1;
-	// 	redir_r = ft_split(rdl->main_str, '>');
-	// 	redir_r_c = count_matris((void **)redir_r);
-	// 	// printf("redir_r_c %d\n", redir_r_c);
-	// 	rdl->quote_count = malloc(sizeof(t_quote_count) * redir_r_c);
-	// 	while (++i < redir_r_c)
-	// 	{
-	// 		rdl->quote_count[i].left = quote_count_left(redir_r[i]);
-	// 		rdl->quote_count[i].right = quote_count_right(redir_r[i]);
-	// 	}
-	// }
-	// else if (ft_strchr(rdl->main_str, '<') != NULL)
-	// {
-	// 	i = -1;
-	// 	redir_l = ft_split(rdl->main_str, '<');
-	// 	redir_l_c = count_matris((void **)redir_l);
-	// 	// printf("redir_l_c %d\n", redir_l_c);
-	// 	rdl->quote_count = malloc(sizeof(t_quote_count) * redir_l_c);
-	// 	while (++i < redir_l_c)
-	// 	{
-	// 		rdl->quote_count[i].left = quote_count_left(redir_l[i]);
-	// 		rdl->quote_count[i].right = quote_count_right(redir_l[i]);
-	// 	}
-	// }
 	int i;
-	int j;
-	int len;
-	int next;
+	int findl;
+	int findr;
 	int find;
-	int l_find;
-	int r_find;
-	int c;
 	int c_n;
+	int c_p;
+	int c;
+	char *str;
 
 	i = 0;
-	j = 0;
-	c = 0;
-	c_n = 0;
-	r_find = 0;
-	l_find = 0;
+	findl = 0;
+	findr = 0;
 	find = 0;
-	len = 0;
-	next = 0;
+	str = NULL;
 	while (i < rdl->len)
 	{
 		c = rdl->main_str[i];
 		c_n = rdl->main_str[i + 1];
-		if ((c == '\"' && c_n != '\"') || (c == '\'' && c_n != '\''))
-			len++;
-		i++;
-	}
-	rdl->quote_count = malloc(sizeof(t_quote_count) * len / 2);
-	i = 0;
-	while (i < rdl->len)
-	{
-		c = rdl->main_str[i];
-		c_n = rdl->main_str[i + 1];
-		if ((c == '\"' && c_n != '\"') || (c == '\'' && c_n != '\''))
+		c_p = rdl->main_str[i - 1];
+		if ((c == '\"' || c == '\'') && c_n != '\"' && find == 0)
 		{
-			if (find == 0)
-				l_find = i;
-			else if (find == 1)
-			{
-				r_find = i;
-				printf("ft_substr: %s\n", ft_substr(rdl->main_str, l_find, r_find));
-				printf("quote_count_left %d : \n", quote_count_left(ft_substr(rdl->main_str, l_find, r_find - l_find)));
-				printf("quote_count_right %d : \n", quote_count_right(ft_substr(rdl->main_str, l_find, r_find - l_find)));
-				rdl->quote_count[j].left = quote_count_left(ft_substr(rdl->main_str, l_find, r_find - l_find));
-				rdl->quote_count[j].right = quote_count_right(ft_substr(rdl->main_str, l_find, r_find - l_find));
-				find = 0;
-				next = 0;
-				j++;
-			}
-			find++;
+			findl = i + 1;
+			find = 1;
 		}
-		i++;
-	}
-	i = 0;
-	printf("len %d\n", len);
-	while (i < len / 2)
-	{
-		printf("rdl->quote_count[%d].left\n", rdl->quote_count[i].left);
-		printf("rdl->quote_count[%d].right\n", rdl->quote_count[i].right);
+		else if ((c == '\"' || c == '\'') && c_p != '\'' && find == 1)
+		{
+			findr = i;
+			str = ft_substr(rdl->main_str, findl, findr - findl);
+			printf("substr : %s \n", str);
+			find = 0;
+		}
+
 		i++;
 	}
 
-	parser_arg(rdl);
+	// parser_arg(rdl);
 }
