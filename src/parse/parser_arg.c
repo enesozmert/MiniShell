17b3764bc1cc18a7ffdc13c9fdf1cda51a8c696b
cagriyalniz <cagriyalniz@gmail.com>
2022-09-12 15:33:26 +0300

    i am pro

--- src/parse/parser_arg.c
+++ src/parse/parser_arg.c
@@ -42,56 +42,55 @@ void parser_arg(t_rdl *rdl)
 	parser_add(rdl, str[0]);
 	i++;
 	while (s[i])
-	{
-		j = 0;
+	{	j = 0;
 		if (s[i] == '\'')
 		{
-			printf("testif 1");
+			//printf("testif 1 ");
 			i++;
 			while (s[i] != '\'')
 			{
 				rdl->buffer[j++] = s[i];
 				i++;
 			}
-			i++;
-			rdl->buffer[j] = '\0';
+ 			rdl->buffer[j] = '\0';
 			parser_add(rdl, rdl->buffer);
 		}
 		else if (s[i] == '\"')
 		{
-			printf("testif 2");
+			//printf("testif 2 ");
 			i++;
 			while (s[i] != '\"')
 			{
 				rdl->buffer[j++] = s[i];
 				i++;
 			}
-			i++;
 			//printf("i: %d\n", i);
 			rdl->buffer[j] = '\0';
 			parser_add(rdl, rdl->buffer);
 		}
 		else if (is_operator(rdl, s[i]))
 		{
-			printf("testif 3");
+			//printf("testif 3");
 			parser_add_operator(rdl, s[i]);
 		}
 		else if (s[i] != '\'' && s[i] != '\"' && s[i] > 32)
 		{
-			printf("testif 3");
+			printf("testif 4 ");
 			printf("s[i] : %c i:%d\n", s[i], i);
-			while (s[i] != '\"' && s[i] != '\'' )
+	
+			while (s[i] != '\"' && s[i] != '\'' && s[i] != '\0')
 			{
 				rdl->buffer[j++] = s[i];
 				i++;
 			}
 			rdl->buffer[j] = '\0';
-			i++;
-			printf("son else if: %s\n", rdl->buffer);
+
+			printf("son else if: %s i: %d\n", rdl->buffer, i);
 			parser_add(rdl, rdl->buffer);
 		}
+		
 			i++;
 
-			
+
 	}
 }
