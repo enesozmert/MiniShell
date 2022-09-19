43ca0f6af3c463dae188678a1eadcc9c9e97c7f3
cagriyalniz <cagriyalniz@gmail.com>
2022-09-19 17:02:55 +0300

    command_create added but missing

--- src/command/command_create.c
+++ src/command/command_create.c
@@ -6,14 +6,43 @@
 /*   By: cyalniz <cyalniz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 14:48:35 by cyalniz           #+#    #+#             */
-/*   Updated: 2022/09/19 16:55:14 by cyalniz          ###   ########.fr       */
+/*   Updated: 2022/09/19 17:02:36 by cyalniz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/header.h"
 
-int command_creade(t_rdl *rdl)
+int command(t_rdl *rdl)
 {
+    int i;
+
+	i = -1;
+	while (++i < token_size(rdl->token))
+	{
+        command_create(rdl);
+		get_next_token(&rdl->token);
+	}
     
-    
+}
+
+int command_create(t_rdl *rdl)
+{
+    if (ft_strncmp(rdl->token->type, "keyword", ft_strlen("keyword")))
+    {
+        rdl->command_list->type = "keyword";
+        rdl->command_list->arg = rdl->token->context;
+/*         rdl->command_list->option = 
+        rdl->command_list->f =  */
+    }
+    else if (ft_strncmp(rdl->token->type, "arg", ft_strlen("arg")))
+    {
+        rdl->command_list->type = "arg";
+        rdl->command_list->arg = rdl->token->context;
+/*         rdl->command_list->option = 
+        rdl->command_list->f =  */
+    }
+    //optionsı alamadığımız için burada ekletemiyoruz :(
+
+
+        
 }
