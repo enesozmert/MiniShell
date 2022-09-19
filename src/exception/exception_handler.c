9e293964719b0e0f9263c36a4eaa47c03059b597
enesozmert <en-es111@hotmail.com>
2022-09-19 15:50:08 +0300

    redir syntax added

--- src/exception/exception_handler.c
+++ src/exception/exception_handler.c
@@ -16,14 +16,15 @@ int	exception_handler(t_rdl *rdl)
 {
 	int			i;
 	int			handle_code;
-	t_exception	exception[4];
+	t_exception	exception[5];
 
 	i = 0;
 	handle_code = 0;
 	exception[0] = (t_exception){100, "quote", keyword_quoute_syntax};
 	exception[1] = (t_exception){101, "command not found", keyword_syntax};
 	exception[2] = (t_exception){102, "quote", quote_syntax};
-	exception[3] = (t_exception){-1, NULL, NULL};
+	exception[3] = (t_exception){103, "parse error near", redir_syntax};
+	exception[4] = (t_exception){-1, NULL, NULL};
 	while (exception[i].message != NULL)
 	{
 		handle_code = exception[i].f(rdl);
