5996ebc63fc53f1eb1a49f081ae8b3fd3b49e499
enesozmert <en-es111@hotmail.com>
2022-09-19 16:55:19 +0300

    token_add update

--- src/token/token_add.c
+++ src/token/token_add.c
@@ -59,14 +59,14 @@ void	token_add_type(t_token **token)
 	t_rdl  *rdl;
 	t_token	*token_cpy;
 
-	i = 0;
+	i = -1;
 	rdl = malloc(sizeof(t_rdl));
 	rdl->keyword_list = malloc(sizeof(t_keyword) * 8);
 	rdl->operator_list = malloc(sizeof(t_operator) * 8);
 	keyword_list(rdl->keyword_list);
 	operator_list(rdl->operator_list);
 	token_cpy = *token;
-	while (i < token_size(*token))
+	while (++i < token_size(*token))
 	{
 		if (is_keyword(rdl, token_cpy->context))
 			token_cpy->type = ft_strdup("keyword");
@@ -74,7 +74,6 @@ void	token_add_type(t_token **token)
 			token_cpy->type = ft_strdup("operator");
 		else
 			token_cpy->type = ft_strdup("arg");
-		i++;
 		token_cpy = token_cpy->next;
 	}
 	token_cpy = *token;
