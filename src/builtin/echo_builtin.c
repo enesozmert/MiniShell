68d885ac3c3d42067815c05049bb6cb3f31adb94
enesozmert <en-es111@hotmail.com>
2022-09-26 11:29:47 +0300

    echo builtin added 1

--- src/builtin/echo_builtin.c
+++ src/builtin/echo_builtin.c
@@ -2,17 +2,17 @@
 
 int echo_start(t_command command)
 {
-    // (void)command;
-    int i;
-    int arg_count;
+    (void)command;
+    // int i;
+    // int arg_count;
 
-    i = -1;
-    arg_count = 2;
-    while (++i < arg_count)
-    {
+    // i = -1;
+    // arg_count = 2;
+    // while (++i < arg_count)
+    // {
         printf("%s\n", command.arg[i]);
-    }
-    return (0);
+    // }
+    // return (0);
 }
 
 //end
