77b268ec2b5459be3a9805fe24c4daea138d9990
enesozmert <en-es111@hotmail.com>
2022-09-28 11:53:34 +0300

    echo builtin update

--- src/builtin/echo_builtin.c
+++ src/builtin/echo_builtin.c
@@ -9,7 +9,7 @@ int echo_start(t_command command)
     arg_count = 2;
     while (++i < arg_count)
     {
-        printf("%s ", command.arg[0]);
+        printf("%s", command.arg[0]);
     }
     printf("\n");
     return (0);
