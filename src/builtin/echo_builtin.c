32e8946c2225df5dd07c4e06f1819df0a256562f
enesozmert <en-es111@hotmail.com>
2022-09-28 11:53:06 +0300

    command create update and echo builtin update

--- src/builtin/echo_builtin.c
+++ src/builtin/echo_builtin.c
@@ -2,16 +2,16 @@
 
 int echo_start(t_command command)
 {
-    // (void)command;
-    // int i;
-    // int arg_count;
+    int i;
+    int arg_count;
 
-    // i = -1;
-    // arg_count = 2;
-    // while (++i < arg_count)
-    // {
-        printf("%s\n", command.arg[0]);
-    // }
+    i = -1;
+    arg_count = 2;
+    while (++i < arg_count)
+    {
+        printf("%s ", command.arg[0]);
+    }
+    printf("\n");
     return (0);
 }
 
--- src/command/command_create.c
+++ src/command/command_create.c
@@ -17,6 +17,7 @@ int command(t_rdl *rdl)
     int i;
 
 	i = -1;
+    // (void)rdl;
     command_malloc(rdl);
 	while (++i < token_size(rdl->token))
 	{
@@ -24,15 +25,15 @@ int command(t_rdl *rdl)
 		get_next_token(&rdl->token);
 	}
     command_run(rdl);
-    i = -1;
-    while (++i < 8)
-    {
-        printf("type : %s |", rdl->command_list[i].type);
-        printf("arg : %s |", rdl->command_list[i].arg[0]);
-        printf("option : %s |", rdl->command_list[i].option);
-        printf("count : %d |", rdl->command_list[i].count);
-        printf("\n");
-    }
+    // i = -1;
+    // while (++i < 1)
+    // {
+    //     printf("type : %s |", rdl->command_list[i].type);
+    //     printf("arg : %s |", rdl->command_list[i].arg[0]);
+    //     printf("option : %s |", rdl->command_list[i].option);
+    //     printf("count : %d |", rdl->command_list[i].count);
+    //     printf("\n");
+    // }
     return (0);
 }
 
@@ -41,13 +42,15 @@ int command_malloc(t_rdl *rdl)
     int command_id;
     int arg_count;
 
-    command_id = 0;
+    command_id = command_find(rdl, rdl->token->context);
     arg_count = token_arg_count(rdl->token);
     if (ft_strncmp(rdl->token->type, "keyword", ft_strlen("keyword")) == 0)
     {
         command_id = command_find(rdl, rdl->token->context);
+        printf("command_id %d\n", command_id);
         rdl->command_list[command_id].arg = (char **)ft_calloc(arg_count, sizeof(char *));
         rdl->command_list[command_id].count++;
+        printf("ok\n");
     }
     return (0);
 }
@@ -56,26 +59,19 @@ int command_create(t_rdl *rdl)
 {
     int command_id;
     int arg_id;
-    int arg_count;
-    //şimdi header.h da command kısmında arg double poniter tutmalıyız
-    //evet
-    //şimdi çoklu mesaj yazdırmak vs lazım
-    //double pointer olunca *arg string oluyor ya
-    //command_id = 0;
+
     arg_id = 0;
     command_id = command_find(rdl, rdl->token->context);
-    arg_count = token_arg_count(rdl->token);
     if (ft_strncmp(rdl->token->type, "keyword", ft_strlen("keyword")) == 0)
     {
         command_id = command_find(rdl, rdl->token->context);
-        printf("arg_count %d\n", arg_count);
-        rdl->command_list[command_id].count++;
+        //printf("command_id %d\n", command_id);
     }
     else if (ft_strncmp(rdl->token->type, "arg", ft_strlen("arg")) == 0)
     {
-        command_id = command_find(rdl, rdl->token->context);
-        if(!rdl->command_list[command_id].arg[arg_id])
-            rdl->command_list[command_id].arg[arg_id] = ft_strdup(rdl->token->context);
+        //printf("command_id %d\n", command_id);
+        rdl->command_list[command_id].arg[arg_id] = malloc(sizeof(char *) * rdl->token->len);
+        rdl->command_list[command_id].arg[arg_id] = rdl->token->context;
         arg_id++;
     }
 /*     else if(ft_strncmp(rdl->token->type, "operator", ft_strlen("operator")) == 0)
