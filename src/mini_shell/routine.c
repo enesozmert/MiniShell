4e38ebe26d62b50ff23c31f89752fe7b4cbc5958
cagriyalniz <cagriyalniz@gmail.com>
2022-10-06 16:50:16 +0300

    syntax sıkıntı yoksa komut calistiriliyor

--- src/mini_shell/routine.c
+++ src/mini_shell/routine.c
@@ -6,7 +6,7 @@
 /*   By: cyalniz <cyalniz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 10:22:50 by cyalniz           #+#    #+#             */
-/*   Updated: 2022/10/06 14:17:10 by cyalniz          ###   ########.fr       */
+/*   Updated: 2022/10/06 16:38:19 by cyalniz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void routine()
 		if (!check_white_space(rdl->main_str))
 		{
 			lexical_analizer(rdl);
-			if(syntax(rdl) == -1)
-				//exit(0);//?
+			if(syntax(rdl) != -1)
+				command(rdl);
 			find_pipe_count(rdl);//new
-			command(rdl);
+			
 			my_add_history(rdl->main_str);
 			keywords_clear(rdl->keyword_list);
 			token_clear(&rdl->token);
