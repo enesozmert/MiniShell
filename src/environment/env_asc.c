b67d7e18cd01dcbf24e98c009d1fa6a0ec1a8aaa
cagriyalniz <cagriyalniz@gmail.com>
2022-10-24 15:43:37 +0300

    env_asc env_dsc completed

--- src/environment/env_asc.c
+++ src/environment/env_asc.c
@@ -2,25 +2,29 @@
 
 int env_asc(char **env)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 	char *temp;
 
+	j = 0;
+	i = 0;
+	temp = NULL;
 	while (env[i + 1])
 	{
-		if (env[i][j] < env[i + 1][j])
+		if (ft_tolower(env[i][j]) > ft_tolower(env[i + 1][j]))
 		{
 			temp = ft_strdup(env[i]);
 			env[i] = ft_strdup(env[i + 1]);
 			env[i + 1] = ft_strdup(temp);
 			i = 0;
 		}
-		else if (env[i][j] == env[i + 1][j])
-		{
+		else if (ft_tolower(env[i][j]) == ft_tolower(env[i + 1][j]))
 			j++;
-		}
 		else
+		{
+			j = 0;
 			i++;
+		}
 	}
 	g_env.env = env;
 	return (0);
