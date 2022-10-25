a6c304a1d0871171b26c2a7b8ee9831353e90fad
enesozmert <en-es111@hotmail.com>
2022-10-26 00:01:37 +0300

    command exec edited 1

--- src/command/command_exec.c
+++ src/command/command_exec.c
@@ -2,15 +2,16 @@
 
 int command_execv(t_command command)
 {
-    printf("ok\n");
     int     result;
     char    *path;
     char    *type[2];
 
     type[0] = ft_strdup(command.type);
     type[1] = NULL;
+    path = command_find_path(type[0]);
 	result = execve(path, type, g_env.env);
-    free(type);
+    free(type[0]);
+    free(path);
     if (result == -1)
         return(-1);
     return (0);
