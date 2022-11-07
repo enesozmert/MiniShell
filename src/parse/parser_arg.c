f7b1ae9e619e0fdd38ce1d69789a720aca8b762f
enesozmert <en-es111@hotmail.com>
2022-11-07 23:10:07 +0300

    parser arg edit 2

--- src/parse/parser_arg.c
+++ src/parse/parser_arg.c
@@ -41,6 +41,8 @@ void parser_arg_isnot(int *k, t_rdl *rdl)
 
 	i = *k;
 	j = 0;
+	if (rdl->quote_prop->flag == 1 && rdl->main_str[i] <= 32 && rdl->main_str[i + 1] > 32  && rdl->main_str[i] != '\0')
+			parser_add(rdl, ft_strdup(" "));
 	while (is_quote(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] > 32 && is_operator(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] != '\0' && is_keyword_builtin(rdl, rdl->buffer) == 0)
 	{
 		rdl->buffer[j++] = rdl->main_str[i];
@@ -100,7 +102,7 @@ void parser_arg(t_rdl *rdl)
 			parser_add_operator(rdl, rdl->main_str[i]);
 		else if (is_quote(rdl, rdl->main_str[i]))
 			parser_add_quote(rdl, rdl->main_str[i]);
-		else if (is_operator(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] > 32 && rdl->main_str[i] != '\0')
+		else if (is_operator(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] != '\0')
 			parser_arg_isnot(&i, rdl);
 		if (rdl->quote_prop->flag == 0)
 			i++;
