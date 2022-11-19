eb08f68f0df3b627f3b01fd0a1c8877484f95161
enesozmert <en-es111@hotmail.com>
2022-11-19 13:10:35 +0300

    command_exec edit

--- src/command/command_exec.c
+++ src/command/command_exec.c
@@ -3,14 +3,29 @@
 int command_execv(t_command command)
 {
     pid_t	pid;
+	int 	i;
+	int		size;
     int     result;
     char    *path;
-    char    *type[2];
+    char    **type;
 
-	result = 0;	
-    type[0] = ft_strdup(command.keyword);
-    type[1] = NULL;
-    path = command_find_path(type[0]);
+
+	i = -1;
+	result = 0;
+	size = token_size(command.tokens);
+    path = command_find_path(command.keyword);
+	type = (char**)malloc(sizeof(char *) * 3);
+    type[0] = command_find_path(command.keyword);
+	type[1] = ft_strdup("");
+	type[2] = NULL;
+	while (++i < size)
+	{
+		if (command.tokens->type_id == 9)
+			type[1] = ft_strjoin(type[1], command.tokens->context);
+		get_next_token(&command.tokens);
+	}
+	// printf("type0 : %s\n", type[0]);
+	// printf("type1 : %s\n", type[1]);
 	pid = fork();
 	signal(SIGINT, proc_signal_handler);
 	if (pid == 0)
