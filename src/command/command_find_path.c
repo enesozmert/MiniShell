432d53f028d98c92464fb5ebef653aa55069411c
cagriyalniz <cagriyalniz@gmail.com>
2022-11-20 16:35:58 +0300

    null value check

--- src/command/command_find_path.c
+++ src/command/command_find_path.c
@@ -13,18 +13,22 @@ char *command_find_path(char *keyword)
     path = env_find_value("PATH");
     if (path != NULL)
         paths = ft_split(path, ':');
-    while (paths[++i])
+    if (paths)
     {
-        path = ft_strjoin("/", keyword);
-        new_path = ft_strjoin(paths[i], path);
-        free(path);
-        if (stat(new_path, &s) == 0)
+        while (paths[++i])
         {
-            ft_free_dbl_str(paths);
-            return (new_path);
+            path = ft_strjoin("/", keyword);
+            new_path = ft_strjoin(paths[i], path);
+            free(path);
+            if (stat(new_path, &s) == 0)
+            {
+                ft_free_dbl_str(paths);
+                return (new_path);
+            }
+            free(new_path);
         }
-        free(new_path);
+        ft_free_dbl_str(paths);
     }
-    ft_free_dbl_str(paths);
+
     return (NULL);
 }
