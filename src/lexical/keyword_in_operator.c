47d21fca3030ff15b256a0503b7f06e8fd89a2e1
enesozmert <en-es111@hotmail.com>
2022-11-23 01:28:03 +0300

    keyword_in_operator paramter edit

--- src/lexical/keyword_in_operator.c
+++ src/lexical/keyword_in_operator.c
@@ -1,22 +1,18 @@
 #include "../../include/header.h"
 
-int keyword_in_operator(t_rdl *rdl, char *str, char operator)
+int keyword_in_operator(t_rdl *rdl, int keyword_id, char operator)
 {
     int			i;
 	int			operator_id;
     char		**operators_id;
-    t_keyword	keyword;
 
     if (is_operator(rdl, operator) == 0)
         return (0);
     i = -1;
 	operator_id = -1;
-    keyword = find_keyword(rdl, str);
-    if (keyword.operators_id != NULL)
+    if (rdl->keyword_list[keyword_id].operators_id == NULL)
         return (0);
-    operators_id = ft_split(keyword.operators_id, ',');
-    // if (!operators_id[0])
-    //     return (0);
+    operators_id = ft_split(rdl->keyword_list[keyword_id].operators_id, ',');
 	while (rdl->operator_list[++operator_id].name != NULL)
 	{
 		if (rdl->operator_list[operator_id].sybl[0] == operator)
