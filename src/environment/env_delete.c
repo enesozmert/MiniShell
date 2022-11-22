d91c8134002d8428a9a241bed6265c35aab7df62
cagriyalniz <cagriyalniz@gmail.com>
2022-11-22 14:40:51 +0300

    env_delete fixed

--- src/environment/env_delete.c
+++ src/environment/env_delete.c
@@ -3,11 +3,13 @@
 int env_delete(char *key)
 {
 	int		i;
+    int     j;
     int     del_env_id;
 	int		count;
     char	**new_env;
 
 	i = -1;
+    j = 0;
 	count = env_len(g_env.env); 
     del_env_id = env_find_id(key);
     if (env_exist(key) == 1)
@@ -16,7 +18,7 @@ int env_delete(char *key)
 		while (g_env.env[++i])
         {
             if (del_env_id != i)
-			    new_env[i] = g_env.env[i];
+			    new_env[j++] = g_env.env[i];
         }
 		new_env[i + 1] = NULL;
 		g_env.env = new_env;
