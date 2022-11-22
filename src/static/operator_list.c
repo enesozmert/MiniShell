550051fc9b702def4997b08004dbf1acb90bcdfa
enesozmert <en-es111@hotmail.com>
2022-11-23 01:28:27 +0300

    operator edit

--- src/static/operator_list.c
+++ src/static/operator_list.c
@@ -2,11 +2,8 @@
 
 void	operator_list(t_operator *operator)
 {
-	// operator[0] = (t_operator){"single_quote" ,"\'"};
-	// operator[1] = (t_operator){"double_quote" ,"\""};
 	operator[0] = (t_operator){"redirect_in" ,"<"};
 	operator[1] = (t_operator){"redirect_out" ,">"};
-	// operator[2] = (t_operator){"varible" ,"$"};
 	operator[2] = (t_operator){"pipe" ,"|"};
 	operator[3] = (t_operator){"and" ,"&"};
 	operator[4] = (t_operator){"equal" ,"="};
