f2af0fa487374fccb223b5ac440fd618b7473a15
enesozmert <en-es111@hotmail.com>
2022-11-23 01:27:18 +0300

    env_find_value edit

--- src/environment/env_find.c
+++ src/environment/env_find.c
@@ -4,24 +4,14 @@ char	*env_find_value(char *key)
 {
 	int		i;
 	int		key_len;
-	int		key_id;
-	char	*key_find;
+	int		env_id;
 
 	i = -1;
 	key_len = ft_strlen(key);
-	key_id = env_find_id(key);
-	if (key_id == -1)
+	env_id = env_find_id(key);
+	if (env_id == -1)
 		return (0);
-	while(g_env.env[++i])
-	{
-		key_find = ft_split(g_env.env[i], '=')[0];
-		if (ft_strncmp(key_find, key, ft_strlen(key_find)) == 0)
-		{
-			free(key_find);
-			return (g_env.env[i] + key_len + 1);
-		}
-	}
-	return (0);
+	return (g_env.env[env_id] + key_len + 1);
 }
 
 int	env_find_id(char *key)
