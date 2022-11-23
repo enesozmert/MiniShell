f58579d6b382eb331045b2c9f43a23e2308623bf
enesozmert <en-es111@hotmail.com>
2022-11-23 11:10:49 +0300

    parser edit

--- src/parse/parser_arg_is.c
+++ src/parse/parser_arg_is.c
@@ -7,6 +7,7 @@ int parser_arg_is(int *k, t_rdl *rdl)
 
 	j = 0;
 	i = *k;
+	printf("ok\n");
 	while (is_quote(rdl, rdl->main_str[i]) == 0 && is_dollar(rdl->main_str[i]) == 0 && rdl->main_str[i] != '\0')
 	{
 		rdl->buffer[j++] = rdl->main_str[i++];
@@ -28,7 +29,19 @@ void parser_arg_isnot(int *k, t_rdl *rdl)
 
 	i = *k;
 	j = 0;
-	while (is_quote(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] > 32 && rdl->main_str[i] != '\0' && is_dollar(rdl->main_str[i]) == 0 && is_operator(rdl, rdl->main_str[i]) == 0)
+	printf("ok1\n");
+	while(is_quote(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] > 32 && ft_isalnum(rdl->main_str[i]) == 0 && rdl->main_str[i] != '\0' && is_dollar(rdl->main_str[i]) == 0 && is_operator(rdl, rdl->main_str[i]) == 0)
+	{
+		rdl->buffer[j++] = rdl->main_str[i++];
+		rdl->index_flag = 1;
+	}
+	if (ft_strlen(rdl->buffer) > 0)
+	{
+		rdl->t_flag = 0;
+		rdl->buffer[j] = '\0';
+		parser_add(rdl, rdl->buffer);
+	}
+	while (is_quote(rdl, rdl->main_str[i]) == 0 && rdl->main_str[i] > 32 && ft_isalnum(rdl->main_str[i]) && rdl->main_str[i] != '\0' && is_dollar(rdl->main_str[i]) == 0 && is_operator(rdl, rdl->main_str[i]) == 0)
 	{
 		rdl->buffer[j++] = rdl->main_str[i++];
 		rdl->index_flag = 1;
