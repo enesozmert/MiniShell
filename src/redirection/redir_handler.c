9e468e5812ac782d8667e322197b3902f7c691ab
enesozmert <en-es111@hotmail.com>
2022-11-24 19:04:16 +0300

    redir_handler edit

--- src/redirection/redir_handler.c
+++ src/redirection/redir_handler.c
@@ -1,5 +1,35 @@
 #include "../../include/header.h"
 
+int redir_output(t_rdl *rdl)
+{
+    printf("redir_output\n");
+    (void)rdl;
+    return (1);
+}
+
+int redir_input(t_rdl *rdl)
+{
+    printf("redir_output\n");
+    (void)rdl;
+    return (1);
+}
+
+static int redir_file_name(t_rdl *rdl)
+{
+    // printf("redir_file_name\n");
+    rdl->redir_prop->file_name = ft_strdup(rdl->token->context);
+    return (1);
+}
+
+static int redir_context(t_rdl *rdl)
+{
+    // printf("redir_context\n");
+    if (rdl->token->type_id == 0)
+        return (-1);
+    rdl->redir_prop->context = ft_strdup(rdl->token->context);
+    return (1);
+}
+
 int redir_handler(t_rdl *rdl)
 {
     printf("ok\n");
@@ -11,29 +41,20 @@ int redir_handler(t_rdl *rdl)
     while (++i < len)
     {
         if (rdl->token->context[0] != '>' && rdl->token->next->context[0] == '>')
-            redir_output(rdl);
-        else if (rdl->token->context[0] != '>' && rdl->token->next->context[0] == '>' && rdl->token->next->next->context[0] == '>')
-            redir_output(rdl);
+            redir_context(rdl);
+        // else if (rdl->token->context[0] != '>' && rdl->token->next->context[0] == '>' && rdl->token->next->next->context[0] == '>')
+        //     redir_output(rdl);
+        else if (rdl->token->context[0] != '>' && (rdl->token->next->context[0] != '>' || rdl->token->next->next->context[0] != '>'))
+            redir_file_name(rdl);
         else if (rdl->token->context[0] != '<' && rdl->token->next->context[0] == '<')
-            redir_input(rdl);
-        else if (rdl->token->context[0] != '<' && rdl->token->next->context[0] == '<' && rdl->token->next->next->context[0] == '<')
-            redir_input(rdl);
+            redir_context(rdl);
+        // else if (rdl->token->context[0] != '<' && rdl->token->next->context[0] == '<' && rdl->token->next->next->context[0] == '<')
+        //     redir_input(rdl);
+        else if (rdl->token->context[0] == '<' && rdl->token->next->context[0] != '<' && rdl->token->next->next->context[0] != '<')
+            redir_file_name(rdl);
         get_next_token(&rdl->token);
     }
-    return (1);
-}
-
-int redir_output(t_rdl *rdl)
-{
-    if (rdl->token->type_id == 0)
-        return (-1);
-    rdl->redir_prop->file_name = ft_strjoin(rdl->redir_prop->file_name, rdl->token->context);
+    printf("context : %s\n", rdl->redir_prop->context);
     printf("file name : %s\n", rdl->redir_prop->file_name);
-    return (1);
-}
-
-int redir_input(t_rdl *rdl)
-{
-    (void)rdl;
     return (1);
 }
