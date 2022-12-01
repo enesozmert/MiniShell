b1a841d934a3c4f5f2a5e5413489a677e859c22a
enesozmert <en-es111@hotmail.com>
2022-12-01 17:32:52 +0300

    token_type_is added

--- src/token/token_type_is.c
+++ src/token/token_type_is.c
@@ -40,7 +40,8 @@ int token_type_is_redir(t_rdl *rdl)
 int token_type_is_option(t_rdl *rdl)
 {
     if (rdl->token_type_prop->token->len == 1 &&
-        is_option(rdl->token_type_prop->token->context[0]))
+        is_option(rdl->token_type_prop->token->context[0]) &&
+        rdl->token_type_prop->keyword_id > 7)
         return (1);
     return (0);
 }
