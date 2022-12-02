b566c90d11bdee1f3068cec58f0c2ef9b18d8d78
enesozmert <en-es111@hotmail.com>
2022-12-02 17:34:33 +0300

    parser edit

--- src/parse/parser_arg.c
+++ src/parse/parser_arg.c
@@ -1,37 +1,5 @@
 #include "../../include/header.h"
 
-/* static int is_keyw(int *k, t_rdl *rdl)
-{
-	int i;
-	int tmp_i;
-
-    i = *k;
-	tmp_i = *k;
-	printf("is_keyw: %c\n", rdl->main_str[i]);
-	parser_arg_keyword(&i, rdl);
-	if (ft_strlen(rdl->buffer) > 0)
-		rdl->buffer = keyword_trim(rdl->buffer);
-	if (is_keyword(rdl, rdl->buffer) && ft_strlen(rdl->buffer) > 0)
-	{
-		i = *k;
-
-		*k = i;
-		printf("keyword: %s\n", rdl->buffer);
-		return (1);
-	}
-	else
-	{
-		i = tmp_i;
-		// i--;
-		return 0;
-	}
-	i--;
-	i = *k;
-
-	*k = i;
-	return (0);
-} */
-
 void parser_arg(t_rdl *rdl)
 {
 	int i;
@@ -50,7 +18,7 @@ void parser_arg(t_rdl *rdl)
 		}
 		else if (is_quote(rdl, rdl->main_str[i]))
 			parser_arg_quote(&i, rdl);
-		else if (rdl->main_str[i] > 32 && ft_isalnum(rdl->main_str[i]))
+		else if (ft_isalnum(rdl->main_str[i]))
 			parser_arg_isnot(&i, rdl);
 		else if (is_dollar(rdl->main_str[i]))
 			parser_add_dollar(rdl, rdl->main_str[i]);
--- src/parse/parser_arg_iskeyword.c
+++ src/parse/parser_arg_iskeyword.c
@@ -10,13 +10,10 @@ void parser_arg_iskeyword(int *k, t_rdl *rdl)
 	parser_arg_keyword(&i, rdl);
 
 	if (is_keyword(rdl, rdl->buffer))
-	{
 		parser_add_keyword(rdl, rdl->buffer);
-	}
-	else
+	else if	(is_keyword(rdl, rdl->buffer) == 0)
 	{
 		i = tmp_i;
-		// i--;
 		return ;
 	}
 	i--;
