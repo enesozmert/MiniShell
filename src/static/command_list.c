0a0ca13a26b3987e11268d3b68e32fe993b752b1
enesozmert <en-es111@hotmail.com>
2022-12-02 17:34:43 +0300

    export string added

--- src/static/command_list.c
+++ src/static/command_list.c
@@ -5,7 +5,7 @@ void	command_list(t_command *command)
 	command[0] = (t_command){"builtin", "echo", "3,5,6,7,10,12", NULL, echo_start, 0, 0, 0};
 	command[1] = (t_command){"builtin", "cd", "5,6,10,13", NULL, cd_start, 0, 0, 0};
 	command[2] = (t_command){"builtin", "pwd", "", NULL, pwd_start, 0, 0, 0};
-	command[3] = (t_command){"builtin", "export", "5,6,8,10,11", NULL, export_start, 0, 0, 0};
+	command[3] = (t_command){"builtin", "export", "5,6,8,10,11,12", NULL, export_start, 0, 0, 0};
 	command[4] = (t_command){"builtin", "unset", "5,6,10", NULL, unset_start, 0, 0, 0};
 	command[5] = (t_command){"builtin", "env", "", NULL, env_start, 0, 0, 0};
 	command[6] = (t_command){"builtin", "exit", "", NULL, exit_start, 0, 0, 0};
