841ad61c727bffffb854554a08dfe9e4dfc2ace5
cagriyalniz <cagriyalniz@gmail.com>
2022-12-12 13:08:28 +0300

    space of the end fixed

--- src/parse/parser_arg_space.c
+++ src/parse/parser_arg_space.c
@@ -7,7 +7,8 @@ void parser_arg_space(int *k, t_rdl *rdl)
 	i = *k; 
 	while(rdl->main_str[i] <= 32 && rdl->main_str[i] != '\0')
 		i++;
-	parser_add(rdl, ft_strdup(" "));
+	if (rdl->main_str[i])
+		parser_add(rdl, ft_strdup(" "));
 	i--;
 	*k = i;
 }
