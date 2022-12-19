b5ab2d637e7bd8983b7c24fe4f775f00db5d063a
enesozmert <en-es111@hotmail.com>
2022-12-19 12:34:19 +0300

    main edit

--- src/mini_shell/main.c
+++ src/mini_shell/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cyalniz <cyalniz@student.42.fr>            +#+  +:+       +#+        */
+/*   By: eozmert <eozmert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 12:19:11 by cyalniz           #+#    #+#             */
-/*   Updated: 2022/12/19 12:19:46 by cyalniz          ###   ########.fr       */
+/*   Updated: 2022/12/19 12:34:06 by eozmert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char const *argv[], char **env)
 {
+	(void)argc;
+	(void)argv;
 	env_init(env);
 	g_env.exit_status = 0;
 	routine();
