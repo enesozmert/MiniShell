6c478d7623eb330194fbacf814254c7ed75051f1
enesozmert <en-es111@hotmail.com>
2022-12-19 13:31:39 +0300

    norm

--- src/parse/parser_add_query.c
+++ src/parse/parser_add_query.c
@@ -6,7 +6,7 @@
 /*   By: eozmert <eozmert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:21:29 by eozmert           #+#    #+#             */
-/*   Updated: 2022/12/19 13:29:38 by eozmert          ###   ########.fr       */
+/*   Updated: 2022/12/19 13:31:27 by eozmert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,18 @@ int	parser_add_char_q(t_rdl *rdl)
 	if (ft_isalnum(rdl->main_str[rdl->i]) == 0 && rdl->main_str[rdl->i] > 32)
 	{
 		parser_add_char(rdl, rdl->main_str[rdl->i]);
-		return (1);
-	}
-	return (0);
+        return (1);
+   }
+   return (0);
+}
+
+int parser_arg_space_q(t_rdl *rdl)
+{
+    if (rdl->main_str[rdl->i] <= 32)
+    {
+        if (is_next_redir(rdl->i, rdl->main_str) == 0 && is_next_pipe(rdl->i, rdl->main_str) == 0)
+            parser_arg_space(&rdl->i, rdl);
+        return (1);
+    }
+    return (0);
 }
